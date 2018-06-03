2c3b4b2cffdd9c988442b1dd7c92d44a5f2e969c
Luis Garcia <garcia@yahoo.com>
2018-06-03 17:14:22 -0400

    modify validation syntax

--- app/models/to_do_list.rb
+++ app/models/to_do_list.rb
@@ -1,6 +1,6 @@
 class ToDoList < ApplicationRecord
-  # validates_presence_of :name, :day
+  validates_presence_of :name, :day
 
-  validates_presence_of :name
-  validates_presence_of :day
+  # validates_presence_of :name
+  # validates_presence_of :day
 end 

92a510b339409caa74097bc505b0997c24c240aa
Luis Garcia <garcia@yahoo.com>
2018-06-03 17:39:47 -0400

    Remove comments

--- app/models/to_do_list.rb
+++ app/models/to_do_list.rb
@@ -1,6 +1,3 @@
 class ToDoList < ApplicationRecord
   validates_presence_of :name, :day
-
-  # validates_presence_of :name
-  # validates_presence_of :day
 end 
--- spec/models/to_do_list_spec.rb
+++ spec/models/to_do_list_spec.rb
@@ -1,14 +1,8 @@
 require "rails_helper"
 
 describe ToDoList do
-  # subject {described_class}
   describe "Validations" do
     it {should validate_presence_of :name}
-
-    # it "validates presence of name" do
-      # require 'pry'; binding.pry
-      # expect(ToDoList).to validate_presence_of :name
     it {should validate_presence_of :day}
-    # end
   end
 end

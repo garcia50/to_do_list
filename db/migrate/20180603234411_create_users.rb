bb1432d1cfe8ec29f0fb4e74c7f1690aa45aa64d
Luis Garcia <garcia@yahoo.com>
2018-06-06 19:01:35 -0400

    Modify and create User and ToDoList tables

---
+++ db/migrate/20180603234411_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.timestamps
+    end
+
+    create_table :to_do_lists do |t|
+      t.belongs_to :users, index: true
+      t.string :day
+      t.string :task
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180603203927) do
+ActiveRecord::Schema.define(version: 20180603234411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "to_do_lists", force: :cascade do |t|
+    t.bigint "users_id"
     t.string "day"
-    t.text "name"
+    t.string "task"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_to_do_lists_on_users_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

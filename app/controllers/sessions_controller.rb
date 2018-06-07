96697885d6727f55fa5235b83d5327b73800e42f
Luis Garcia <garcia@yahoo.com>
2018-06-07 12:41:28 -0400

    Create sessions controller with new, create and destroy actions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new;end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash.now[:notice] = "Logged in as #{current_user.email}"
+      redirect_to '/dashboard'
+    else
+      flash.now[:notice] = "email and/or password may not exist or match in our database"
+      render :new
+    end
+  end
+
+  def destroy
+    session.destroy
+    redirect_to root_path
+  end
+end

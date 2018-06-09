2afcbf7459d66c7c86e726fbb262bf9ec5db787e
Luis Garcia <garcia@yahoo.com>
2018-06-09 13:57:50 -0400

    Remove useless code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,10 @@
 class UsersController < ApplicationController
   before_action :check_user, only: [:show]
 
-  def index;end
-
   def show;end
 
   def new
-    @user = User.new()    
+    @user = User.new
   end
 
   def create 

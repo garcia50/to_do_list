fad61cb1f4f6ca3c1c8eaaa89d2b7fada3a934c6
Luis Garcia <garcia@yahoo.com>
2018-06-09 14:02:22 -0400

    Add show/destroy actions to ToDoList class

--- app/controllers/to_do_lists_controller.rb
+++ app/controllers/to_do_lists_controller.rb
@@ -19,6 +19,18 @@ class ToDoListsController < ApplicationController
       render :new
     end
   end
+
+  def show
+    @to_do_list = current_user.to_do_lists.find
+  end
+
+  def destroy
+    @to_do_list = ToDoList.find(params[:id])
+    @to_do_list.destroy
+
+    redirect_to user_to_do_lists_path(current_user)
+  end
+
   private
     def todo_params
       params.require(:to_do_list).permit(:day, :task)

b86ad4a5220b8121a2c0fe45212eb79c17f1b0e0
Luis Garcia <garcia@yahoo.com>
2018-06-09 14:02:04 -0400

    Add new/create/private params actions to ToDoList class

--- app/controllers/to_do_lists_controller.rb
+++ app/controllers/to_do_lists_controller.rb
@@ -4,4 +4,23 @@ class ToDoListsController < ApplicationController
   def index
     @to_do_lists = current_user.to_do_lists 
   end
+
+  def new 
+    @to_do_list = current_user.to_do_lists.new
+  end
+
+  def create
+    @to_do_list = current_user.to_do_lists.new(todo_params)
+    if @to_do_list.save
+      flash[:success] = "Lets get it done!"
+      redirect_to user_to_do_lists_path(current_user)
+    else
+      flash[:error] = "Da HAIL!? Did not save"
+      render :new
+    end
+  end
+  private
+    def todo_params
+      params.require(:to_do_list).permit(:day, :task)
+    end
 end

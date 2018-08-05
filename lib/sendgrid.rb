0314bc4df54cf31681210f5cb2af9090402273a7
Luis Garcia <garcia50@yahoo.com>
2018-08-04 20:28:17 -0400

    Remove sendgrid account detail from lib

--- lib/sendgrid.rb
+++ lib/sendgrid.rb
@@ -10,20 +10,5 @@ from = Email.new(email: 'user@domain.com')
 subject = "Test Email"
 content = Content.new(type: 'text/html', value: body)
 mail = Mail.new(from, subject, to, content)
-sg = SendGrid::API.new(api_key: "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")
+sg = SendGrid::API.new(api_key: #B7 check notes to retrieve api key
 response = sg.client.mail._('send').post(request_body: mail.to_json)
-
-
-# from = Email.new(email: 'test@example.com')
-# to = Email.new(email: 'garcia50@yahoo.com')
-# subject = 'Sending with SendGrid is Fun'
-# content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
-# mail = Mail.new(from, subject, to, content)
-
-# sg = SendGrid::API.new(api_key: "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")
-# response = sg.client.mail._('send').post(request_body: mail.to_json)
-# puts response.status_code
-# puts response.body
-# puts response.headers
-
-# bin/rails runner lib/sendgrid.rb
